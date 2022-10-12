a82a4c71c3f1d874a384bb24e34ed4e9cc30934d
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-12 15:56:07 +0200

    added template literal

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,34 +6,28 @@ const nextButton = document.querySelector('.next');
 
 // array percorso immagini
 const linkArray = [
-    '../assets/img/01.webp',
-    '../assets/img/02.webp',
-    '../assets/img/03.webp',
-    '../assets/img/04.webp',
-    '../assets/img/05.webp',
+    './assets/img/01.webp',
+    './assets/img/02.webp',
+    './assets/img/03.webp',
+    './assets/img/04.webp',
+    './assets/img/05.webp',
 ]
 
 // contatore immagine corrente
-let currentImg = 0;
+let currentIndex = 2;
 
 // creazione degli elementi img e set dell'active img
 for(let i = 0; i < linkArray.length; i++){
-    let imgEl = document.createElement('img');
-    imgEl.src = linkArray[i];
-    slidesContainer.prepend(imgEl);
-    if(i == currentImg) {
-        console.log(imgEl)
-        imgEl.classList.add('active');
-    } else {
-        imgEl.classList.remove('active');
-    }
+    let imgEl = `<img class="${i == currentIndex ? 'active' : ''}" src="${linkArray[i]}">`
+    console.log(imgEl)
+    slidesContainer.insertAdjacentHTML("afterbegin", imgEl);
 
 }
 
 nextButton.addEventListener('click', ()=>{
     console.log('click');
-    currentImg++;
-    console.log(currentImg);
+    currentIndex++;
+    
 
 });
 
